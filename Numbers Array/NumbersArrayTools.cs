41a2d30a9f47b0571ca724ca3132fecd4cb1ee68
Kristaps-M <kristapsmitins@gmail.com>
2023-01-27 14:42:02 +0200

    Add project files.

---
+++ Numbers Array/NumbersArrayTools.cs
@@ -0,0 +1,141 @@
+﻿namespace Numbers_Array
+{
+    public class NumbersArrayTools
+    {
+        private int _arraySize;
+        private int[,] _arr;
+        private int _padding;
+
+        public NumbersArrayTools(int arraySize, int[,] arr, int padding)
+        {
+            _arraySize = arraySize;
+            _arr = arr;
+            _padding = padding;
+        }
+
+        public int findMin()
+        {
+            if (_arr.Length == 0)
+            {
+                throw new Exception("Array is empty");
+            }
+
+            int min = int.MaxValue;
+            for (int col = 0; col < _arraySize; col++)
+            {
+                for (int row = 0; row < _arraySize; row++)
+                {
+                    if (_arr[col, row] < min)
+                    {
+                        min = _arr[col, row];
+                    }
+                }
+            }
+
+            return min;
+        }
+
+        public int findMax()
+        {
+            if (_arr.Length == 0)
+            {
+                throw new Exception("Array is empty");
+            }
+
+            int max = int.MinValue;
+            for (int col = 0; col < _arraySize; col++)
+            {
+                for (int row = 0; row < _arraySize; row++)
+                {
+                    if (_arr[col, row] > max)
+                    {
+                        max = _arr[col, row];
+                    }
+                }
+            }
+
+            return max;
+        }
+
+        public List<List<int>> findCordinates(int numberToCompare)
+        {
+            var list = new List<List<int>>();
+
+            for (int col = 0; col < _arraySize; col++)
+            {
+                //var tempList = new List<int>();
+                for (int row = 0; row < _arraySize; row++)
+                {
+                    if (_arr[col, row] == numberToCompare)
+                    {
+                        list.Add(new List<int>() { row, col });
+                    }
+                }
+            }
+
+            return list;
+        }
+
+        public void printArray(int[,] arr)
+        {
+            var theCols = "COLS........" + new String(' ', _arraySize -10);
+
+            Console.Write("   ROWS........" + new String(' ', _arraySize - 10));
+            for (int col = 0; col < _arraySize; col++)
+            {
+                Console.WriteLine();
+                Console.Write($"{theCols[col]} ");
+                for (int row = 0; row < _arraySize; row++)
+                {
+                    Console.Write(arr[col, row].ToString().PadLeft(_padding).PadRight(_padding));
+                }
+            }
+        }
+
+        public int[,] sortTwoDimensionalArray()
+        {
+            var listOfNumbers = new List<int>();
+
+            for (int col = 0; col < _arraySize; col++)
+            {
+                for (int row = 0; row < _arraySize; row++)
+                {
+                    listOfNumbers.Add(_arr[col, row]);
+                }
+            }
+
+            listOfNumbers.Sort();
+
+            int[,] arrayOfNumbers = new int[_arraySize, _arraySize];
+            var incrementNum = 0;
+            for (int col = 0; col < _arraySize; col++)
+            {
+                for (int row = 0; row < _arraySize; row++)
+                {
+                    arrayOfNumbers[col, row] = listOfNumbers[incrementNum];
+                    incrementNum++;
+                }
+            }
+
+            return arrayOfNumbers;
+        }
+
+        public void printMinAndMaxNumberCordinates(List<List<int>> minNumCords, List<List<int>> maxNumCords)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Min number cordinates! ([row, col])");
+            foreach (var item in minNumCords)
+            {
+                Console.WriteLine("[" + String.Join(", ", item) + "]");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Max number cordinates! ([row, col])");
+            foreach (var item in maxNumCords)
+            {
+                Console.WriteLine("[" + String.Join(", ", item) + "]");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Numbers Array/Program.cs
@@ -0,0 +1,43 @@
+﻿using Numbers_Array;
+
+const int ARRAY_SIZE = 20;
+const int MAX_NUMBER = 100;
+int PADDING = MAX_NUMBER.ToString().Length + 1;
+Console.WriteLine("Creating 2D array!");
+int[,] array = createTwoDimensionalArray();
+
+var superDuper = new NumbersArrayTools(ARRAY_SIZE, array, PADDING);
+Console.WriteLine($"2D array {ARRAY_SIZE}x{ARRAY_SIZE} with random numbers from 0 to {MAX_NUMBER}");
+superDuper.printArray(array);
+var minValue = superDuper.findMin();
+var maxValue = superDuper.findMax();
+var minNumCords = superDuper.findCordinates(minValue);
+var maxNumCords = superDuper.findCordinates(maxValue);
+var sortedArray = superDuper.sortTwoDimensionalArray();
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine($"Min and Max value in array is {minValue} and {maxValue}");
+Console.WriteLine();
+
+superDuper.printMinAndMaxNumberCordinates(minNumCords, maxNumCords);
+
+Console.WriteLine("Sorted 2D array");
+superDuper.printArray(sortedArray);
+
+int[,] createTwoDimensionalArray()
+{
+    var rnd = new Random();
+    int[,] array = new int[ARRAY_SIZE, ARRAY_SIZE];
+
+    for (int col = 0; col < ARRAY_SIZE; col++)
+    {
+        for (int row = 0; row < ARRAY_SIZE; row++)
+        {
+            var randomNumber = rnd.Next(MAX_NUMBER + 1);
+            array[col, row] = randomNumber;
+        }
+    }
+
+    return array;
+}
